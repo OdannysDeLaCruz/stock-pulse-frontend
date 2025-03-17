601578c6fb46a39acf942d6236825da889c16ab5
Odannys Enrique De La Cruz Calvo <el_odanis321@hotmail.com>
2025-03-17 14:50:33 -0500

    feat: crear componente para buscar stock

---
+++ src/components/InputSearch.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="flex items-center mb-10">
+    <div class="relative w-full max-w-md">
+      <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+        <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd"
+            d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+            clip-rule="evenodd" />
+        </svg>
+      </div>
+
+      <!-- search input -->
+      <input type="text" v-model="searchQuery"
+        @input='handleSearch'
+        class="w-full pl-10 pr-10 p-2 rounded-md border-2 border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-500/10 outline-none"
+        placeholder="Search by symbol or company" />
+
+      <!-- clear search button -->
+      <button v-if="searchQuery" @click="clearSearch" class="absolute inset-y-0 right-0 pr-3 flex items-center">
+        <svg class="h-5 w-5 text-gray-400 hover:text-gray-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+          fill="currentColor">
+          <path
+            d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+        </svg>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const emit = defineEmits<{
+  (e: 'search', query: string): void
+  (e: 'clear'): void
+}>()
+
+const searchQuery = ref('')
+
+const handleSearch = () => {
+  emit('search', searchQuery.value)
+}
+
+const clearSearch = () => {
+  searchQuery.value = ''
+  emit('clear')
+}
+</script>
