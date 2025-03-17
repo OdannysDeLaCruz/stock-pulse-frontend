17eb57ae4cc59daa99fc568425f6bea1cd4045a3
Odannys Enrique De La Cruz Calvo <el_odanis321@hotmail.com>
2025-03-17 14:43:07 -0500

    feat: crear componente carousel infinito

---
+++ src/components/menus/BasicMenuItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex justify-center items-center">
+    <button class=" hover:text-blue-800 px-4 py-2">
+      <RouterLink @click='unSelect' :to="`/stocks?filter=${item.slug}`" class="text-black" :class="isActive ? 'link-active' : ''">
+        <span>{{ item.name }}</span>
+      </RouterLink>
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RouterLink, useRoute, useRouter } from 'vue-router'
+
+const props = defineProps<{
+  item: {
+    name: string,
+    slug: string
+  },
+  isActive: boolean
+}>()
+
+const route = useRoute()
+const router = useRouter()
+
+const unSelect = () => {
+  if (props.isActive && route.query.filter) {
+    router.push({path: '/stocks'})
+  }
+}
+</script>
+
+<style scoped>
+.link-active {
+  color: #2563eb;
+  font-weight: bold;
+}
+</style>
---
+++ src/components/stocks/stock-carousel/StockCarouselItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex items-center px-6 border-r hover:bg-gray-700" :class="[
+    stock.analysis.change_value > 0 ? 'border-green-500' : 'border-red-500',
+    stock.analysis.change_value > 0 ? 'text-green-400' : 'text-red-400',
+  ]">
+    <RouterLink :to="`/stocks/${stock.ticker}`">
+      <span class="font-medium">{{ stock.ticker }}</span>
+    </RouterLink>
+    <span class="ml-2">
+      {{ stock.analysis.change_value > 0 ? '+' : '' }}{{ stock.analysis.change_value.toFixed(2) }}%
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Stock } from '@/types'
+import { RouterLink } from 'vue-router'
+
+defineProps<{
+  stock: Stock
+}>()
+</script>
