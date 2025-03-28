43207c46b7e14bfbf07770fbd6dd81e372f15747
Odannys Enrique De La Cruz Calvo <el_odanis321@hotmail.com>
2025-03-28 12:23:20 -0500

    feat: buscar stocks

--- src/views/stocks/StocksPage.vue
+++ src/views/stocks/StocksPage.vue
@@ -1,23 +1,32 @@
 <script setup lang="ts">
-import { ref, computed, onMounted } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 import { useStocksStore } from '@/stores/stocks'
 import { useMenusFilterStockStore } from '@/stores/menusFilterStock'
 
-import StockList from '@/components/stocks/StockTableList.vue'
+import StockTableList from '@/components/stocks/StockTableList.vue'
 import StockHorizontal from '@/components/stocks/stock-vertical/StockListVertical.vue'
 import BasicMenu from '@/components/menus/BasicMenu.vue'
 import StockSearch from '@/components/InputSearch.vue'
+import type { Stock } from '@/types'
 
 const stocksStore = useStocksStore()
 const menusFilterStock = useMenusFilterStockStore()
 
 const searchQuery = ref('')
+const filteredStocks = ref<Stock[]>([])
 
-onMounted(() => {
-  stocksStore.fetchStocks()
-  stocksStore.fetchRecommendedStocks()
-  stocksStore.fetchNotRecommendedStocks()
+onMounted(async () => {
+  await stocksStore.fetchStocks()
+  await stocksStore.fetchRecommendedStocks()
+  await stocksStore.fetchNotRecommendedStocks()
+
+  filteredStocks.value = stocksStore.stocks
 })
+
+const fetchMoreStocks = async (page: number) => {
+  await stocksStore.fetchStocks(page)
+  filteredStocks.value = stocksStore.stocks
+}
 
 const handlerClear = () => {
   searchQuery.value = ''
@@ -27,15 +36,33 @@ const handleSearch = (query: string) => {
   searchQuery.value = query
 }
 
-// Computed property para filtrar los stocks
-const filteredStocks = computed(() => {
-  if (!searchQuery.value) return stocksStore.stocks
+// Watcher para buscar stocks
+watch(searchQuery, async (newValue) => {
+  if (newValue) {
+    const query = newValue.toLowerCase()
 
-  const query = searchQuery.value.toLowerCase()
-  return stocksStore.stocks.filter(stock =>
-    stock.ticker.toLowerCase().includes(query) ||
-    stock.company.toLowerCase().includes(query)
-  )
+    filteredStocks.value = stocksStore.stocks.filter(stock =>
+      stock.ticker.toLowerCase().includes(query) ||
+      stock.company.toLowerCase().includes(query)
+    )
+
+    if (filteredStocks.value.length) {
+      return filteredStocks
+    } else {
+      // Si no hay resultados, realizar una búsqueda en la API
+      const queryResponse = await stocksStore.searchStock(query)
+      console.log(queryResponse)
+
+      if (queryResponse) {
+        filteredStocks.value = queryResponse
+        queryResponse.forEach(stock => {
+          stocksStore.setStock(stock)
+        })
+      }
+    }
+  } else {
+    filteredStocks.value = stocksStore.stocks
+  }
 })
 </script>
 <template>
@@ -48,20 +75,45 @@ const filteredStocks = computed(() => {
 
         <BasicMenu class='mb-8' :menuItems="menusFilterStock.items" />
 
-        <StockList :stocks="filteredStocks" />
+        <StockTableList :stocks="filteredStocks" />
+
+        <!-- Paginator -->
+        <div class="w-full flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+          <div class="w-full flex flex-1 justify-between">
+            <a href="#"
+              v-if='stocksStore.page > 1'
+              @click='fetchMoreStocks(stocksStore.page - 1)'
+              class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Anterior
+            </a>
+            <a href="#"
+              class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">
+              {{ stocksStore.page }} </a>
+            <a href="#"
+              @click='fetchMoreStocks(stocksStore.page + 1)'
+              class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Siguiente
+            </a>
+          </div>
+        </div>
       </div>
       <!-- side -->
       <div class='col-span-3 bg-white rounded-xl shadow-lg h-screen'>
-        <!-- Recommended stocks -->
-        <div class="bg-white rounded-xl mb-6" v-if='stocksStore.recommendedStocks.length'>
-          <h2 class="text-lg font-bold mb-4 p-4">Recommendations for today</h2>
-          <StockHorizontal v-for="stock in stocksStore.recommendedStocks" :key="stock.id" :stock="stock" />
-        </div>
-        <!-- No recommended stocks -->
-        <div class="bg-white rounded-xl shadow-lg p-4" v-if='stocksStore.notRecommendedStocks.length'>
-          <h2 class="text-lg font-bold">No recommended stocks</h2>
-          <StockHorizontal v-for="stock in stocksStore.notRecommendedStocks" :key="stock.id" :stock="stock" />
-        </div>
+        <template v-if='stocksStore.recommendedStocks.length || stocksStore.notRecommendedStocks.length'>
+          <!-- Recommended stocks -->
+          <div class="bg-white rounded-xl mb-6" v-if='stocksStore.recommendedStocks.length'>
+            <h2 class="text-lg font-bold mb-4 p-4">Recommendations for today</h2>
+            <StockHorizontal v-for="stock in stocksStore.recommendedStocks" :key="stock.id" :stock="stock" />
+          </div>
+          <!-- No recommended stocks -->
+          <div class="bg-white rounded-xl shadow-lg p-4" v-if='stocksStore.notRecommendedStocks.length'>
+            <h2 class="text-lg font-bold">No recommended stocks</h2>
+            <StockHorizontal v-for="stock in stocksStore.notRecommendedStocks" :key="stock.id" :stock="stock" />
+          </div>
+        </template>
+        <template v-else>
+          <div class="bg-gray-100 rounded-xl h-[100px] flex items-center justify-center p-4 m-5">
+            Nada que ver por aquí.
+          </div>
+        </template>
       </div>
     </div>
 
